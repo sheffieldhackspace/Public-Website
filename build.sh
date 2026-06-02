f893e67c8650897db850466e9a0712a511983431
alifeee <alifeee@alifeee.net>
2026-06-02 18:34:09 +0100

    add build script to simplify actions

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "remove existing _site"
+rm -rf _site
+mkdir _site
+
+echo "build blog (see blog README for how to install build tools)"
+(
+    cd blog
+    bundle exec jekyll build
+    npx -y pagefind --site _site
+)
+
+echo "move blog to _site"
+cp -r blog/_site/* _site
+
+echo "move homepage to _site"
+cp -r homepage/* _site
